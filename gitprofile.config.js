d881ad172c3686a24b0bf576ad0377373818057d
AshiqSazid <www.ashiqsazid494@gmail.com>
2025-08-07 23:38:04 +0600

    fresh push

---
+++ gitprofile.config.js
@@ -0,0 +1,185 @@
+// gitprofile.config.js
+
+const config = {
+  github: {
+    username: 'Ashiq Sazid', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 8, // How many projects to display.
+    exclude: {
+      forks: false, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    linkedin: 'trahmanrifat',
+    twitter: '',
+    mastodon: '',
+    facebook: 'profile.php?id=100010343971752',
+    instagram: 'i.m.tahmid',
+    youtube: '', // example: 'pewdiepie'
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: '',
+    stackoverflow: '', // example: '1/jeff-atwood'
+    skype: '',
+    telegram: '',
+    website: 'https://tahmid37.github.io/',
+    phone: '+8801739596331',
+    email: 'Tahmid.Rahman@brainstation-23.com',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'Python',
+    'Django',
+    'Fastapi',
+    'Natural Language Processing (NLP)',
+    'Langchain',
+    'LlamaIndex',
+    'Node.js',
+    'MySQL',
+    'PostgreSQL',
+    'Git',
+    'Docker',
+    'Latex',
+    'LLM',
+    'Chatbots',
+    'Pandas',
+  ],
+  experiences: [
+    {
+      company: 'Brain station 23',
+      position: 'Associate Software Engineer',
+      from: 'December 2023',
+      to: 'Present',
+      companyLink: 'https://brainstation-23.com/',
+    },
+    {
+      company: 'Brain station 23',
+      position: 'Data and Ai intern',
+      from: 'May 2023',
+      to: 'July 2023',
+      companyLink: 'https://brainstation-23.com/',
+    },
+  ],
+  /* certifications: [
+    {
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com'
+    },
+  ], */
+  education: [
+    {
+      institution: 'Islamic University of Technology(IUT)',
+      degree: 'Bachelor of Science in Computer Science',
+      from: '2020',
+      to: '2024',
+    },
+    
+  ],
+
+  // To hide the `My Projects` section, keep it empty.
+  externalProjects: [
+    {
+      title: '',
+      description:
+        '',
+      imageUrl: '',
+      link: '',
+    },
+    {
+      title: '',
+      description:
+        '',
+      imageUrl: '',
+      link: '',
+    },
+  ],
+  // Display blog posts from your medium or dev account. (Optional)
+  blog: {
+    source: 'medium', // medium | dev
+    username: '@tahmidrahman_41692', // to hide blog section, keep it empty
+    limit: 1, // How many posts to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'lemonade',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Made with <a 
+      class="text-primary" href="https://tahmid37.github.io/"
+      target="_blank"
+      rel="noreferrer"
+    >GitProfile</a> and ❤️`,
+};
+
+export default config;
